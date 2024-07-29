f3caca2f0f58020370562f2a0d63a713a39c0d07
IGsayonara <diduniki@gmail.com>
2024-07-29 22:31:18 +0100

    eslint: lint all files

--- src/api/giphy/instance.ts
+++ src/api/giphy/instance.ts
@@ -1,8 +1,8 @@
-import axios from "axios";
+import axios from 'axios'
 
 export const giphyAxios = axios.create({
-    baseURL: "https://api.giphy.com/v1",
-    params: {
-        api_key: import.meta.env.VITE_GIHPY_API_KEY
-    }
+  baseURL: 'https://api.giphy.com/v1',
+  params: {
+    api_key: import.meta.env.VITE_GIHPY_API_KEY,
+  },
 })
