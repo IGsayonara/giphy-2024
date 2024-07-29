7c06d39db444fa00f39ee012ee3ff85d40c126fa
IGsayonara <diduniki@gmail.com>
2024-07-29 22:32:57 +0100

    add search store

--- src/stores/search.store.ts
+++ src/stores/search.store.ts
@@ -1,25 +1,26 @@
-import {defineStore} from 'pinia';
-import {MultiResponse} from "giphy-api";
-import {fetchRandomResults, fetchSearchResults} from "../api/giphy/utils.ts";
+import { defineStore } from 'pinia'
+import { MultiResponse } from 'giphy-api'
+import { fetchRandomResults, fetchSearchResults } from '../api/giphy/utils.ts'
 
 export const useSearchStore = defineStore('search', {
-    state: () => ({
-        searchValue: "" as string,
-        searchResults: null as null | MultiResponse['data'],
-        pagination: null as null | MultiResponse['pagination'],
-    }),
-    getters: {},
-    actions: {
-        async changeSearchValue(newSearchValue: string) {
-            this.searchValue = newSearchValue;
-            await this.fetchSearchResults();
-        },
-        async fetchSearchResults() {
-            if (this.searchValue) {
-                this.searchResults = await fetchSearchResults(this.searchValue).then((r) => r.data);
-            } else {
-                this.searchResults = await fetchRandomResults();
-            }
-        }
+  state: () => ({
+    searchValue: '' as string,
+
+    searchResults: null as null | MultiResponse['data'],
+    pagination: null as null | MultiResponse['pagination'],
+  }),
+  getters: {},
+  actions: {
+    async changeSearchValue (newSearchValue: string) {
+      this.searchValue = newSearchValue
+      await this.fetchSearchResults()
     },
-});
+    async fetchSearchResults () {
+      if (this.searchValue) {
+        this.searchResults = await fetchSearchResults(this.searchValue).then(r => r.data)
+      } else {
+        this.searchResults = await fetchRandomResults()
+      }
+    },
+  },
+})
