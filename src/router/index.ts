5dc0d325473f6166edb250a328609d3d818bed64
IGsayonara <diduniki@gmail.com>
2024-07-31 09:49:46 +0100

    try memory history

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,12 +5,12 @@
  */
 
 // Composables
-import { createRouter, createWebHashHistory } from 'vue-router/auto'
+import { createMemoryHistory, createRouter } from 'vue-router/auto'
 import { setupLayouts } from 'virtual:generated-layouts'
 import { routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createMemoryHistory(import.meta.env.BASE_URL),
   routes: setupLayouts(routes),
 })
 
