d034eb533d088b6e291ed75b3867b720f15a6b61
IGsayonara <diduniki@gmail.com>
2024-07-31 10:06:21 +0100

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/giphy-2024/sw.js', { scope: '/giphy-2024/' })})}
