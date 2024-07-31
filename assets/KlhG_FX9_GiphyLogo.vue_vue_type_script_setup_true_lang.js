3f0c0ac7f03b91ce7e2807e9139caf4f91139123
IGsayonara <diduniki@gmail.com>
2024-07-31 15:03:34 +0100

    Updates

---
+++ assets/KlhG_FX9_GiphyLogo.vue_vue_type_script_setup_true_lang.js
@@ -0,0 +1,1 @@
+import{f as e}from"./CPa9649O_color.js";import{V as s}from"./CVGFgVrw_VImg.js";import{d as a,z as n,o as r,c as p}from"./BSTPl1k5_index.js";const c="/giphy-2024/assets/Blvzu1MO_logo.png",g=a({__name:"GiphyLogo",setup(m){const t=n(null);return e("Giphy").then(o=>o.data).then(o=>{t.value=o}),(o,_)=>(r(),p(s,{height:"50px",src:c}))}});export{g as _};
