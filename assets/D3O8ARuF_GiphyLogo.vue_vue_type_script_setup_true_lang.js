6415f66288104aa10157f3d512fcbf2e001e742d
IGsayonara <diduniki@gmail.com>
2024-07-31 14:58:48 +0100

    Updates

---
+++ assets/D3O8ARuF_GiphyLogo.vue_vue_type_script_setup_true_lang.js
@@ -0,0 +1,1 @@
+import{f as e}from"./BPV8WmPz_color.js";import{V as s}from"./NdEq_ycf_VImg.js";import{d as a,z as n,o as r,c as p}from"./CqCg048u_index.js";const c="/giphy-2024/assets/Blvzu1MO_logo.png",g=a({__name:"GiphyLogo",setup(m){const t=n(null);return e("Giphy").then(o=>o.data).then(o=>{t.value=o}),(o,_)=>(r(),p(s,{height:"50px",src:c}))}});export{g as _};
